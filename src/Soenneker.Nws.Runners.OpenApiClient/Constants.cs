369b22a9da32d85d462647c940c2e85f2f0608ea
Jake Soenneker <jake@soenneker.com>
2026-03-16 08:38:13 -0500

    Initial commit

---
+++ src/Soenneker.Nws.Runners.OpenApiClient/Constants.cs
@@ -0,0 +1,6 @@
+namespace Soenneker.Nws.Runners.OpenApiClient;
+
+public static class Constants
+{
+    public const string Library = "Soenneker.Nws.OpenApiClient";
+}
---
+++ test/Soenneker.Nws.Runners.OpenApiClient.Tests/NwsOpenApiClientRunnerTests.cs
@@ -0,0 +1,19 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Nws.Runners.OpenApiClient.Tests;
+
+[Collection("Collection")]
+public sealed class NwsOpenApiClientRunnerTests : FixturedUnitTest
+{
+    public NwsOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
