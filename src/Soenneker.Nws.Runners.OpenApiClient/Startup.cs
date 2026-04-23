6b174ff4da2fbd56c21ff675bf5840c988433abb
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 11:36:23 -0500

    lifetime issues

--- src/Soenneker.Nws.Runners.OpenApiClient/Startup.cs
+++ src/Soenneker.Nws.Runners.OpenApiClient/Startup.cs
@@ -21,10 +21,10 @@ public static class Startup
     public static IServiceCollection SetupIoC(this IServiceCollection services)
     {
         services.AddHostedService<ConsoleHostedService>()
-                .AddScoped<IFileOperationsUtil, FileOperationsUtil>()
-                .AddRunnersManagerAsScoped()
-                .AddCloudflareDownloaderAsScoped()
-                .AddKiotaUtilAsScoped();
+                .AddSingleton<IFileOperationsUtil, FileOperationsUtil>()
+                .AddRunnersManagerAsSingleton()
+                .AddCloudflareDownloaderAsSingleton()
+                .AddKiotaUtilAsSingleton();
 
         return services;
     }
